cf9c211f84b70b8731463a092b4c387c8c8e7b84
Enes <eenesbariss@gmail.com>
2023-05-10 22:50:38 +0300

    Taslak
    Bunun ustune yazabilirsiniz

--- proje.py
+++ proje.py
@@ -1,3 +1,23 @@
-       
+class Sozluk:
+    def __init__(self):
+        with open('proje.txt', 'r') as dosya:
+            satirlar = dosya.readlines()
+        self.sozluk = {}
+        for satir in satirlar:
+            kelime, tanim = satir.strip().split(":")
+            self.sozluk[kelime]=tanim
+        self.kelime_defteri = []
+    
+    def cumleEkle():
+        
+    def yorumEkle():
+    
+    def kelime_bilgi_goster(self):
+        kelime=input("lutfen aratmak istediginiz kelimeyi giriniz:")
+        if kelime in self.sozluk:
+            print(f"{kelime} kelimesi için bilgiler:{self.sozluk[kelime]}")  
 def main():
-    kelime =
+    sozluk=Sozluk()
+    sozluk.kelime_bilgi_goster()
+    
+main()
